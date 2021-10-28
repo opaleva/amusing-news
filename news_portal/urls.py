2d01b34de80a84b2aee0bce2692dda8021c56ca0
opaleva <from.aldebaran@gmail.com>
2021-10-28 21:30:10 +0500

    day 14. project update

--- news_portal/urls.py
+++ news_portal/urls.py
@@ -1,5 +1,5 @@
 """news_portal URL Configuration"""
-from django.conf.urls.i18n import i18n_patterns
+# from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.urls import path, include
@@ -12,10 +12,19 @@ sitemaps = {
     'articles': ArticleSitemap
 }
 
-urlpatterns = i18n_patterns(
+urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('articles.urls')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
          name='django.contrib.sitemaps.views.sitemap'),
     path('__debug__/', include(debug_toolbar.urls)),
-)
+]
+
+# urlpatterns = i18n_patterns(
+#     path('admin/', admin.site.urls),
+#     path('', include('articles.urls')),
+#     path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
+#          name='django.contrib.sitemaps.views.sitemap'),
+#     path('__debug__/', include(debug_toolbar.urls)),
+# )
+
